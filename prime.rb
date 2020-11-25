fe44d62c356d0a644fb229eb16c953fcb1aa843c
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 18:28:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,13 +3,10 @@ require 'pry'
 
 def prime?(number)
   f = 2
-  if number > f
+  while number > f
     return false
     if number % f <= 0
       f += 1
     end
-  elsif number < 1
-    return false
-  end
   true
 end
