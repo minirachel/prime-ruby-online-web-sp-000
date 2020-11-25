e2ff181ccff09b34044e1e8559be64aea623c496
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 18:12:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,16 @@
 require 'pry'
 
+number = -1
+
 def prime?(number)
 #trial_division
   f = 2
   while number > f
-    binding.pry
     return false if number % f == 0
       f += 1
     end
   true
 end
 
-number = -1
+
 prime?(number)
