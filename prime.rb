8acabfe4652bbf164ded658098c25d1761f2c89f
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 18:16:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,16 @@ require 'pry'
 number = -1
 
 def prime?(number)
-#trial_division
-  f = 2
-  while number > f
-    return false if number % f == 0
-      f += 1
-    end
+  (2..(num-1)).each do |n|
+    return false if num % n == 0
+  end
+  true
+
+
+
+  # f = 2
+  # while number > f
+  #   return false if number % f == 0
+  #     f += 1
+  #   end
 end
