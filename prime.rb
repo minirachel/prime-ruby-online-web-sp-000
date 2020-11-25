a019e569f722386f74ea0c64dfcc81643fff619a
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 18:37:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+require 'pry'
+
+
+def prime?(number)
+  return false if number < 2
+
+  (2..number/2).none?{|i| number % i == 0}
+
+end
+
+  # if number > f
+  #   return false
+  #   if number % f <= 0
+  #     f += 1
+  #   end
+  # elsif number < 2
+  #   return false
+  # elsif
+  #   true
+  # end
