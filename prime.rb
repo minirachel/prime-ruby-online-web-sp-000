6b2f0b0abee1b703e384956cb1d0942ff21931ef
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 18:19:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,10 @@
 require 'pry'
 
+
 def prime?(number)
-  (2..(number-1)).each do |n|
-    return false if number % n == 0
-  end
-  true
-
-
-
-  # f = 2
-  # while number > f
-  #   return false if number % f == 0
-  #     f += 1
-  #   end
+  f = 2
+  while number > f
+    return false if number % f <= 0
+      f += 1
+    end
 end
