4208c851e16fdde66b104b12a53317e8ca927f6c
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 18:34:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,19 @@ require 'pry'
 
 
 def prime?(number)
-  f = 2
-  if number > f
-    return false
-    if number % f <= 0
-      f += 1
-    end
-  elsif number < 1
-    return false
-  elsif
-    true
-  end
+  return false if number < 2
+
+  (2..number/2).none?{|i| n % i == 0}
+
 end
+
+  # if number > f
+  #   return false
+  #   if number % f <= 0
+  #     f += 1
+  #   end
+  # elsif number < 2
+  #   return false
+  # elsif
+  #   true
+  # end
